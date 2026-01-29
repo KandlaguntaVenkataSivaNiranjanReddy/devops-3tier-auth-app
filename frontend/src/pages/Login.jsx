89d06db0854fb27dc987eb8365c0071adc74f958
KandlaguntaVenkataSivaNiranjanReddy <117029570+KandlaguntaVenkataSivaNiranjanReddy@users.noreply.github.com>
2026-01-29 20:49:31 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,11 +2,12 @@ import axios from "../api/axios";
 
 export default function Login() {
   const login = async () => {
-    const res = await axios.post("/auth/login", {
+    const res = await axios.post("/login", {
       email: "test@test.com",
       password: "123456"
     });
     alert(res.data.message);
   };
+
   return <button onClick={login}>Login</button>;
 }
